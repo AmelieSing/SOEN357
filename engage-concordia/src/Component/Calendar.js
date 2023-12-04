761770f4aadd2f90a4c5ca7728b45bdf6b562ae8
AmelieSing <77857217+AmelieSing@users.noreply.github.com>
2023-12-04 18:15:33 -0500

    - Adding example Calendar

--- engage-concordia/src/Component/Calendar.js
+++ engage-concordia/src/Component/Calendar.js
@@ -3,7 +3,43 @@ import { useLocation } from 'react-router-dom';
 import React, { useEffect, useState } from 'react';
 import axios from 'axios';
 import profilePic from './CSS/images/profile_pic.jpg';
+import ChatUnclickedIcon from './CSS/images/chat_unclicked.svg';
+import SendUnclickedIcon from './CSS/images/send_unclicked.svg';
 
+
+import './CSS/profile.css';  // Import the CSS file directly
+import "./CSS/chat-box.css";
+
+
+export const ChatUnclicked = ({ onChatClick }) => {
+  return (
+    <div className="chat-unclicked" onClick={onChatClick}>
+      <img className="vector" alt="Vector" src={ChatUnclickedIcon} />
+    </div>
+  );
+};
+export const SendUnclicked = ({ onShareClick }) => {
+  return (
+    <div className="send-unclicked" onClick={onShareClick}>
+      <img className="vector" alt="Vector" src={SendUnclickedIcon} />
+    </div>
+  );
+};
+
+export const ChatBox = ({ onClose }) => {
+  return (
+    <div className="chat-box">
+      <div className="header">
+        <button className="close-button" onClick={onClose}>
+          X
+        </button>
+      </div>
+      <div className="content">
+        {/* Your chat content goes here */}
+      </div>
+    </div>
+  );
+};
 
 const Calendar = () =>{
   const location = useLocation();
@@ -12,7 +48,8 @@ const Calendar = () =>{
   const userId = params.get('userId');
   const [user, setUser] = useState(null);
   const [hasError, setHasError] = useState(false);
-  const [errorMessage, setErrorMessage] = useState("")
+  const [errorMessage, setErrorMessage] = useState("");
+  const [popupVisible, setBoxVisible] = useState(false);
 
   const [currentDate, setCurrentDate] = useState(new Date());
   const [newEvent, setNewEvent] = useState({
@@ -23,6 +60,14 @@ const Calendar = () =>{
     end_time: "",
     description: ""
   });
+
+  const handleChatClick = () => {
+    setBoxVisible((prev) => !prev);
+  };
+
+  const closePopup = () => {
+    setBoxVisible(false);
+  };
 
 
   const addEvent = async () => {
@@ -535,10 +580,35 @@ if (modal2) {
 <dialog className='modalDetails' id="detailsModal">
   <div className='details-container'>
   <button id="closeDetailsModal">X</button>
-  <div className="example-caution">*This is only an example, this is not using real data from the server.*</div>             
   
+  <p className="event-title-2">A Christmas Carol - a dramatic reading with musical guest</p>
+              <div className="event-specific-2">
+                <div className="event-details-2">
+                  <div className="right-side-col-2">
+                    <div className="time">
+                      <div className="text-wrapper-3">7:00PM - 10:00PM</div>
+                    </div>
+                    <div className="date">December 17, 2024</div>
+                    <div className="actionable-2">
+                    <ChatUnclicked onChatClick={handleChatClick}/>
+                      <SendUnclicked className="send-unclicked" />
+                    </div>
+                    {popupVisible && <ChatBox onClose={closePopup} />} {/* Add this line */}
+                  </div>
+                  <p className="p">
+                    Get ready to be swept away by the magic of the holiday season as our area&#39;s talented performers
+                    bring this timeless tale to the stage. Join us for a delightful evening of festive cheer!
+                  </p>
+                  <div className="status-2">
+                    <div className="text-wrapper-7">30 going</div>
+                    <div className="text-wrapper-5">5 not going</div>
+                    <div className="text-wrapper-8">30 interested</div>
+                  </div>
+                </div>
+                
+              </div>
+              <div className="example-caution">*This is only an example, this is not using real data from the server.*</div>             
   </div>
-  
 </dialog>
 
 <dialog className='modal' id="slctDateModal">
